4d7d8e80b5c34438184cc0995f83a71f912e774f
leocalmeida <leonardocaetanoalmeida@gmail.com>
2019-05-30 14:23:11 -0300

    Start

---
+++ js/principal.js
@@ -0,0 +1,31 @@
+var pesoValido = true;
+var alturaValido = true;
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var peso = tdPeso.textContent;
+if(peso>=1000 || peso<=0){
+  tdImc.textContent = "Peso inválido"
+  pesoValido=false;
+}
+
+
+var altura = tdAltura.textContent;
+if(altura>=3 || altura<=0){
+  tdImc.textContent = "Altura inválida"
+  alturaValido=false;
+}
+
+
+
+if(pesoValido && alturaValido){
+    var imc = peso/(altura*altura);
+    tdImc.textContent = imc;
+    console.log(imc);
+}
