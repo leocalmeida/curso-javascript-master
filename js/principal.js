540a5d0afc77c55144118d24f646a461de8cfba6
leocalmeida <leonardocaetanoalmeida@gmail.com>
2019-06-01 08:15:44 -0300

    SSH - Sem senha

--- js/principal.js
+++ js/principal.js
@@ -43,3 +43,4 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente")
 botaoAdicionar.addEventListener("click", function() {
   console.log("cliquei no botao")
 })
+//leonardo SSH testando
